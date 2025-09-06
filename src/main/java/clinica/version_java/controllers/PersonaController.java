873ea90f565cd8feae224aa0151faf302f766d4a
Ricardo Pavel Presentacion <pardovelcari111@gmail.com>
2025-09-05 23:28:34 -0600

    refactor: correccion de anotaciones adecuadas para los endpoint y creacion de endpoints mas claros para su uso

--- src/main/java/clinica/version_java/controllers/PersonaController.java
+++ src/main/java/clinica/version_java/controllers/PersonaController.java
@@ -20,9 +20,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,10 +50,18 @@ public class PersonaController {
         }
     }
 
-    // no jala
-
-    @PostMapping("/update")
+    @PutMapping("/update")
     public ResponseEntity<?> actualizar(@RequestBody DTOPersona persona) {
+        try {
+            persona = personaService.guardarOActualizarPersonaCompleta(persona);
+            return ResponseEntity.status(HttpStatus.OK).body(persona);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestBody DTOPersona persona) {
         try {
             persona = personaService.guardarOActualizarPersonaCompleta(persona);
             return ResponseEntity.status(HttpStatus.OK).body(persona);
@@ -98,7 +108,7 @@ public class PersonaController {
         return personaService.obtenerAntecedentes(id);
     }
 
-    @PostMapping("/eliminarContactoEmergencia")
+    @DeleteMapping("/eliminarContactoEmergencia")
     public ResponseEntity<?> eliminarContactoEmergencia(@RequestBody Map<String, Integer> body) {
         if (personaService.eliminarContactoEmergencia((Integer) body.get("idPaciente"),
                 (Integer) body.get("idContacto")))
@@ -106,7 +116,7 @@ public class PersonaController {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
 
-    @PostMapping("/eliminarAntecedenteFamiliar")
+    @DeleteMapping("/eliminarAntecedenteFamiliar")
     public ResponseEntity<?> eliminarAntecedenteFamiliar(@RequestBody Map<String, Integer> body) {
         if (personaService.eliminarAntecedenteFamiliar((Integer) body.get("idPaciente"),
                 (Integer) body.get("idAntecedente")))
@@ -114,7 +124,7 @@ public class PersonaController {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
 
-    @PostMapping("/actualizarCorreo")
+    @PutMapping("/actualizarCorreo")
     public ResponseEntity<?> actualizarCorreo(@RequestBody Map<String, String> body) {
         int idPersona = Integer.parseInt(body.get("idPersona"));
         String correoNuevo = body.get("correoNuevo");
@@ -126,7 +136,7 @@ public class PersonaController {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
 
-    @PostMapping("/actualizarTelefono")
+    @PutMapping("/actualizarTelefono")
     public ResponseEntity<?> actualizarTelefono(@RequestBody Map<String, String> body) {
         int idPersona = Integer.parseInt(body.get("idPersona"));
         String telefonoNuevo = body.get("telefonoNuevo");
@@ -134,6 +144,25 @@ public class PersonaController {
         Estado estado = Estado.valueOf(body.get("estado"));
 
         if (personaService.actualizarTelefono(telefonoNuevo, telefonoViejo, idPersona, estado))
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+
+    @DeleteMapping("/borrarCorreo")
+    public ResponseEntity<?> borrarCorreo(@RequestBody Map<String, String> body) {
+        int idPersona = Integer.parseInt(body.get("idPersona"));
+        String correoAntiguo = body.get("correoAntiguo");
+
+        if (personaService.actualizarCorreo(correoAntiguo, correoAntiguo, idPersona, Estado.INACTIVO))
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+
+    @DeleteMapping("/borrarTelefono")
+    public ResponseEntity<?> borrarTelefono(@RequestBody Map<String, String> body) {
+        int idPersona = Integer.parseInt(body.get("idPersona"));
+        String telefonoViejo = body.get("telefonoViejo");
+        if (personaService.actualizarTelefono(telefonoViejo, telefonoViejo, idPersona, Estado.INACTIVO))
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
